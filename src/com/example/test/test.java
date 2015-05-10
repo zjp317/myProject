202658e4cae6f3aeb912aeeb00460d5d0fa596f1
zjp317 <zjp317@163.com>
2015-05-10 21:29:18 +0800

    first commit

---
+++ src/com/example/test/test.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+public class test {
+	
+	public static void main(String[] args)
+	{
+		
+		boolean y=true;
+		String s2=String.valueOf(y);
+		boolean b=Boolean.getBoolean(s2);
+		System.out.println("aaa");		
+	}
+
+}
